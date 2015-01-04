26ef03d999386b9e1de8780396fa2f5173c9ff0a
jweppler <harlin01@hotmail.com>
2015-01-04 14:27:29 -0600

    Added Drop Downs
    Added drop downs for enums shirt size and hat size and common html
    helper method

---
+++ SecretSantaDraw/HtmlHelperExtensions/HtmlDropDownExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using SecretSantaDraw.Models;
+
+namespace SecretSantaDraw.HtmlHelperExtensions
+{
+    public static class HtmlDropDownExtensions
+    {
+        public static MvcHtmlString EnumDropDownList<TEnum>(this HtmlHelper htmlHelper, string name, TEnum selectedValue)
+        {
+            IEnumerable<SelectListItem> values = Enum.GetValues(typeof(TEnum))
+            .Cast<TEnum>().Select(v => new SelectListItem
+                {
+                    Text = v.ToString(),
+                    Value = v.ToString(),
+                    Selected = (v.Equals(selectedValue))
+                });
+
+            return htmlHelper.DropDownList(
+                name,
+                values
+                );
+        }
+
+    }
+}
