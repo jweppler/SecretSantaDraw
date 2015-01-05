041f3055e90cccfe8c88861b003697a756a060e0
jweppler <harlin01@hotmail.com>
2015-01-05 15:30:38 -0600

    Updated Views to use/display new Profile properties
    Updated views to use/display new Profile properties
    Cleaned up Html helpers for better enum support
    Set Simple Authentication to use database

--- SecretSantaDraw/HtmlHelperExtensions/HtmlDropDownExtensions.cs
+++ SecretSantaDraw/HtmlHelperExtensions/HtmlDropDownExtensions.cs
@@ -12,7 +12,7 @@ namespace SecretSantaDraw.HtmlHelperExtensions
 {
     public static class HtmlDropDownExtensions
     {
-/*        public static MvcHtmlString EnumDropDownList<TEnum>(this HtmlHelper htmlHelper, string name, TEnum selectedValue)
+        public static MvcHtmlString EnumDropDownList<TEnum>(this HtmlHelper htmlHelper, string name, TEnum selectedValue)
         {
             IEnumerable<SelectListItem> values = Enum.GetValues(typeof(TEnum))
             .Cast<TEnum>().Select(v => new SelectListItem
@@ -28,61 +28,23 @@ namespace SecretSantaDraw.HtmlHelperExtensions
                 values
                 );
         }
-*/
-
-        public static MvcHtmlString EnumDropDownList<TEnum>(this HtmlHelper htmlHelper, string name, TEnum selectedValue)
-        {
-            IEnumerable<TEnum> values = Enum.GetValues(typeof(TEnum))
-            .Cast<TEnum>();
-
-            List<SelectListItem> items = new List<SelectListItem> {};
-            Type type = typeof(TEnum);
-            object[] attributes = new object[] { };
-            string displayName;
-            foreach (var v in values)
-            {
-                var memberInfo = type.GetMember(v.ToString());
-                attributes = new object[] {};
-                displayName = null;
-                if (memberInfo.Length > 0)
-                    attributes = memberInfo[0].GetCustomAttributes(typeof(DisplayAttribute), false);
-                if(attributes.Length > 0)
-                    displayName = ((DisplayAttribute)attributes[0]).Name;
-                items.Add(new SelectListItem()
-                {
-                    Text = displayName != null ? displayName : v.ToString(),
-                    Value = v.ToString(),
-                    Selected = (v.Equals(selectedValue))
-                });
-            }
-
-            return htmlHelper.DropDownList(
-                name,
-                items
-                );
-        }
 
         public static MvcHtmlString EnumDisplayName<TEnum>(this HtmlHelper htmlHelper, string name, TEnum value)
         {
-            Type type = typeof(TEnum);
+            return new MvcHtmlString(GetDisplayAttributeFrom(value, typeof(TEnum)));
+        }
+
+        public static string GetDisplayAttributeFrom<TEnum>(this TEnum value, Type type)
+        {
             string displayName = null;
             var attributes = new object[] { };
             var memberInfo = type.GetMember(value.ToString());
-            if (memberInfo.Length > 0)
+            if (memberInfo.Any())
                 attributes = memberInfo[0].GetCustomAttributes(typeof(DisplayAttribute), false);
-            if (attributes.Length > 0)
+            if (attributes.Any())
                 displayName = ((DisplayAttribute)attributes[0]).Name;
 
-            return new MvcHtmlString(displayName != null ? displayName : value.ToString());
-        }
-
-        public static string GetDisplayAttributeFrom(this Enum enumValue, Type type)
-        {
-            var memberInfo = type.GetMember(enumValue.ToString());
-            var attributes = memberInfo[0].GetCustomAttributes(typeof(DisplayAttribute), false);
-            var description = ((DisplayAttribute)attributes[0]).Name;
-
-            return description != null ? description : enumValue.ToString();
+            return displayName != null ? displayName : value.ToString();
         }
 
     }
